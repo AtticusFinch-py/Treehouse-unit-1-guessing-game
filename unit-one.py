de9ea8cd1cc53bab90256dc299cbde7be754319a
AtticusFinch-py <pat.bain7@gmail.com>
2021-03-29 15:24:59 -0500

    Update unit-one.py

--- unit-one.py
+++ unit-one.py
@@ -7,21 +7,31 @@ def start_game():
 	correct_answer = random.randrange(1, 11)
 
 	attempts = 0
+	
+
 
 	while True:
 		try:
 			user_guess = int(input("Please pick a number between 1 and 10: "))
+
 			if user_guess <=0 or user_guess >10:
 				print("Sorry, the number must be between 1 and 10. Try again...")
+
 		except ValueError:
 			print("Please enter an integer")
+
 		else:
 			attempts += 1
 
 			if user_guess == correct_answer:
 				print("You got it! It only took you {} attempts".format(attempts))
-				break
+				play_again = input("Would you like to play again? [y/n]: ".lower())
+				
 
+				while play_again == "y":
+					correct_answer = random.randrange(1, 11)		
+				
+				
 			elif user_guess > correct_answer:
 				print("Guess lower")
 				continue
@@ -32,7 +42,5 @@ def start_game():
 
 	return attempts
 	
-
-
 start_game()
 
